3c82e9b74df7ac124250b04c6ac156acda2afae6
1501814877@qq.com <1501814877@qq.com>
2017-07-31 16:09:11 +0800

    完成九宫格 Signed-off-by: 1501814877@qq.com <1501814877@qq.com>

--- JggListView/index.android.js
+++ JggListView/index.android.js
@@ -10,9 +10,13 @@ import {
     StyleSheet,
     Text,
     View,
-    ListView
+    ListView,
+    Image,
+    TouchableOpacity
 } from 'react-native';
 var requestData = require('./shareData.json');
+var windowWidth=require('Dimensions').get('window').width;
+var column=3;
 var JggListView = React.createClass({
     getInitialState(){
         //1.设置数据源
@@ -23,25 +27,49 @@ var JggListView = React.createClass({
         }
     },
     render(){
-        return {
+        return (
             <ListView
                 dataSource={this.state.dataSource}//数据源
                 renderRow={this.renderRow}
+                contentContainerStyle={{
+                    flexDirection:'row',
+                    flexWrap:'wrap',
+                    alignItems:'flex-start',
+                }}
             ></ListView>
-        }
+        )
     },
     renderRow(rowData, sectionID, rowID, highlightRow){
-        return ()
+        return (
+            <TouchableOpacity
+                activeOpacity={0.5}
+            >
+                <View style={styles.container}>
+                    <Image
+                        source={{uri:rowData.icon}}
+                        style={{
+                            width: windowWidth/column*0.6,
+                            height:windowWidth/column*0.6,
+                            marginTop:10
+                        }}
+                    ></Image>
+                    <Text
+                        style={{
+                            fontSize:20,
+                        }}
+                    >{rowData.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
     }
 
 });
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        flexDirection:'column',
+        width:windowWidth/column,
+        alignItems:'center'
     },
     welcome: {
         fontSize: 20,
